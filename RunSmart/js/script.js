142678667d464cd2753ad92cf4f7ae94ccca8e64
XyRmA-322 <102918873+XyRmA-322@users.noreply.github.com>
2022-04-04 23:10:45 +0500

    Add files via upload

--- RunSmart/js/script.js
+++ RunSmart/js/script.js
@@ -1,17 +1,48 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick slick-next"></button>',
+        responsive:[
+            {
+                breakpoint: 991.97,
+                settings: {
+                    draggable: true,
+                }
+            },{
+                breakpoint: 576.97,
+                settings: {
+                    arrows: false,  
+                    draggable: true,
+                }
+            }
+                
+        ]
     });
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(clas){
+        $(clas).each(function(i){
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    
+            })       
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__link-list');
+
+  });  
+
+
+
