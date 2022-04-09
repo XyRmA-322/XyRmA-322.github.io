534fd4c329d25b372a5e4be3135c97ada019bca2
XyRmA-322 <102918873+XyRmA-322@users.noreply.github.com>
2022-04-09 16:19:14 +0500

    Add files via upload

--- Clock/js/script.js
+++ Clock/js/script.js
@@ -11,5 +11,23 @@ setInterval(() => {
     hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
     mn.style.transform = `rotateZ(${mm}deg)`;
     sc.style.transform = `rotateZ(${ss}deg)`;
+
+    let hour = document.getElementById('hour');
+    let min = document.getElementById('min');
+    let sec = document.getElementById('sec');
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    h = (h < 10) ? "0" + h : h
+    m = (m < 10) ? "0" + m : m
+    s = (s < 10) ? "0" + s : s
+ 
+    hour.innerHTML = h;
+    min.innerHTML = m;
+    sec.innerHTML = s;
+
+
 });
 
